9e8f34a228fafc997afaf003bc270a472683786a
aarabdh <aarabdhtiwari@gmail.com>
2024-09-29 01:03:10 +0530

    Changes to enable github actions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-}
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'barbaad-dev'; // Replace with your actual GitHub repository name
 
-module.exports = nextConfig
+const nextConfig = {
+  output: 'export',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+};
+
+module.exports = nextConfig;
