6cbe11e886303af3490cc8997490c1ba88714587
aarabdh <aarabdhtiwari@gmail.com>
2025-02-15 19:20:03 +0530

    Imrpovements on send email as well as navbar and mypic.

--- app/components/MyPic.tsx
+++ app/components/MyPic.tsx
@@ -5,7 +5,7 @@ export default function MyPic() {
   return (
     <section className='w-full mx-auto'>
         <img 
-            className = 'border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8'
+            className = 'border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8 z-0'
             src = "/images/my_photo.jpg"
             width={200}
             height={200}
