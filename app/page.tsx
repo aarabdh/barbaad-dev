a8c6ed19affd99b964aa06e3b4fc5f875f3dd723
aarabdh <aarabdhtiwari@gmail.com>
2025-07-27 01:02:40 +0530

    Adding my resume to the site :)

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,41 @@
 import Link from "next/link";
 import MyPic from "./components/MyPic";
-import Posts from "./components/Posts";
 
 export default function Home() {
-	return (
-		<div>
-			<div className="px-6 mt-7 mx-auto max-w-screen-md">
-				<main className="border border-gray-300 rounded-lg p-4">
-					<MyPic />
-					<p className="mt-12 mb-3 text-3xl text-center dark:text-white">
-						Hello there, I&apos;m <span className="font-bold">Aarabdh</span>!
-					</p>
-					<p className="mb-12 text-xl text-center dark:text-white">
-						This site is under maintenance,
-						<br />
-						but feel free to browse around!
-						<br /><br />
-						Wanna contact me? <br />
-						<Link href="/contact" className="underline">Hit me up!</Link>
-					</p>
-					<div className="ml-6 mb-5">
-						<Link href="https://clicks.barbaad.dev/" className="underline text-2xl dark:text-white/90">
-							Clicks</Link>
-					</div>
-					<div className="ml-6 mb-5">
-						<Link href="/game-of-life" className="underline text-2xl dark:text-white/90">
-							Conway&apos;s Game of Life</Link>
-					</div>
-					<div className="ml-6 mb-5">
-						<Link href="/posts" className="underline text-2xl dark:text-white/90">
-							Blog Posts</Link>
-					</div>
-				</main>
-			</div>
-			
-		</div>
-	);
+  return (
+    <div>
+      <div className="px-6 mt-7 mx-auto max-w-screen-md">
+        <main className="border border-gray-300 rounded-lg p-4">
+          <MyPic />
+          <p className="mt-12 mb-3 text-3xl text-center dark:text-white">
+            Hello there, I&apos;m <span className="font-bold">Aarabdh</span>!
+          </p>
+          <p className="mb-12 text-xl text-center dark:text-white">
+            This site is under maintenance,
+            <br />
+            but feel free to browse around!
+            <br /><br />
+            Wanna contact me? <br />
+            <Link href="/contact" className="underline">Hit me up!</Link>
+          </p>
+          <div className="ml-6 mb-5">
+            <Link href="https://clicks.barbaad.dev/" className="underline text-2xl dark:text-white/90">
+              Clicks</Link>
+          </div>
+          <div className="ml-6 mb-5">
+            <Link href="/game-of-life" className="underline text-2xl dark:text-white/90">
+              Conway&apos;s Game of Life</Link>
+          </div>
+          <div className="ml-6 mb-5">
+            <Link href="/posts" className="underline text-2xl dark:text-white/90">
+              Blog Posts</Link>
+          </div>
+          <div className="ml-6 mb-5">
+            <Link href="/resume" className="underline text-2xl dark:text-white/90">
+              My Resume</Link>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
